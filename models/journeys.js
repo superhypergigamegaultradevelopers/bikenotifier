5b44542175116be33933b0ac319b856c6a795937
Zenchy <alvaro.corral.soilan@gmail.com>
2019-02-12 08:49:19 +0100

    Foto, de fondo menu, trabajar en ramas

--- models/journeys.js
+++ models/journeys.js
@@ -5,6 +5,7 @@ const journeySchema = new Schema
 ({
     checkpoints:[],//desde api de google
     identifier:String, //ya veremos como entra
+    //textbox
     markIni: {type: Schema.Types.ObjectId, ref: "Marks"},//ver como entra desde Marks
     markEnd: {type: Schema.Types.ObjectId, ref: "Marks"},//ver como entra desde Marks
     kindOfJourney:{type:String, enum:['Job journey','Pleassure journey','Buying journey','Just for a walk']}
