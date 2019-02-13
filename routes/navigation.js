9153aed9e54846110526f0820034e83f7e749744
Zenchy <alvaro.corral.soilan@gmail.com>
2019-02-13 08:38:43 +0100

    Conseguido pasar markers, hacer formulario de journeys

--- routes/navigation.js
+++ routes/navigation.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const passport = require('passport');
 const router = express.Router();
+const bodyParse=require('body-parser')
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const marks = require('../models/marks.js')
+
 
 // router.get("/",(req,res,next)=>{
 //     res.render("tracking/map")
@@ -9,6 +12,18 @@ const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
 
 router.get("/trips",ensureLoggedIn("/"),(req,res,next)=>{
 //render la pagina de trip
+})
+
+router.post("/navi",bodyParse(),(req,res,next)=>{
+    console.log("va o no va",typeof( req.body.marks))
+    marks.create(
+    //    // { _id: mark._id }, 
+       {location: {lat:req.body.marks,lng:req.body.marks3}}
+    //   //  done
+    //   );
+    
+    )
+    
 })
 
 // router.get("/trip",midelweeeeer,(req,res,next)=>{
@@ -23,16 +38,16 @@ router.get("/trips",ensureLoggedIn("/"),(req,res,next)=>{
 //FILTER
 // })
 
- router.get("/newpost",midelweeeeer,(req,res,next)=>{
+ router.get("/newpost",(req,res,next)=>{
 //Nos tiene que llevar a un formulario donde escribirmos un post nuevo
  })
 
-router.post("/newpost",midelweeeeer,(req,res,next)=>{
+router.post("/newpost",(req,res,next)=>{
 //Recoge de un formulario
  })
 
 
- router.get("/posts/:_id",midelweeeeer,(req,res,next)=>{
+ router.get("/posts/:_id",(req,res,next)=>{
 //points of interest
  })
 
