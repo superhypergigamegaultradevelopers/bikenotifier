37c020234350e09ef200443fc78e401195a7658e
Zenchy <alvaro.corral.soilan@gmail.com>
2019-02-14 13:36:10 +0100

    refactor maps

--- public/javascripts/maps.js
+++ public/javascripts/maps.js
@@ -1,85 +1,70 @@
-const center= { lat: 40.4167, lng: -3.7042 }
+const center= {lat: 40.4167,lng: -3.7042}
 const lat=40.4167
 const lng=-3.7042
 let waypts=[]
 var dirService = new google.maps.DirectionsService;
 var dirDisplay = new google.maps.DirectionsRenderer;
-const madridCentro = 
-{
-  lat: 40.6167,
-  lng: -3.842
-};  
+const madridCentro = {lat: 40.6167,lng: -3.842};  
 var pointer = [];
-
-
   
 function startMap() 
 { 
-     map = new google.maps.Map(document.getElementById('map'),
+  map = new google.maps.Map(document.getElementById('map'),
   {
     zoom: 10,
     center: center
-  });
-  
- 
+  }); 
 }
-//todo: consider moving out this call for additional flexibility
+
 startMap();
 
 map.addListener('click', function (e)
 {
-  
-    new google.maps.Marker(
-    {
-      map: map,
-      position: 
-      {
-        lat: e.latLng.lat(),
-        lng: e.latLng.lng()
-      }
-    })
+  new google.maps.Marker(
+  {
+    map: map,
+    position:{lat: e.latLng.lat(),lng: e.latLng.lng()}
+  })
     
-    var location=e.latLng.lat()
-    var location3=e.latLng.lng()
+  var location=e.latLng.lat()
+  var location3=e.latLng.lng()
 
-    document.querySelector("#marks").value=location
-    document.querySelector("#marks3").value=location3
+  document.querySelector("#marks").value=location
+  document.querySelector("#marks3").value=location3
 
-    if(pointer.length<2){
-pointer.push({lat:location,lng:location3})}
+  if(pointer.length<2)
+  {pointer.push({lat:location,lng:location3})}
 
-waypts.push({
-  location: {lat:location,lng:location3},
-  stopover: true
-});
+  waypts.push(
+  {
+    location: {lat:location,lng:location3},
+    stopover: true
+  });
 
-drawRoute(dirService, dirDisplay);
-
+  drawRoute(dirService, dirDisplay);
 })
 
-
-
-dirDisplay.setMap(map);
-//todo: check this
-// document.getElementById("now").onclick = function () {
-  
-// }
-
-
 dirDisplay.setMap(map);
 
-function drawRoute(dirService, dirDisplay){
-  if(pointer.length > 1) {
-    dirService.route({
+function drawRoute(dirService, dirDisplay)
+{
+  if(pointer.length > 1) 
+  {
+    dirService.route(
+    {
       origin:pointer[0],
       destination: pointer[1],
-     waypoints: waypts,
+      waypoints: waypts,
       optimizeWaypoints: true,
-       travelMode: 'BICYCLING'
-    }, function (response, status) {
-      if (status === 'OK') {
+      travelMode: 'BICYCLING'
+    },
+   function (response, status)
+    {
+      if (status === 'OK') 
+      {
         dirDisplay.setDirections(response);
-      } else {
+      } else 
+      {
         window.alert('Directions request failed due to ' + status);
       }
     });
